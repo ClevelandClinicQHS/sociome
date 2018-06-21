ff5f626296d107da9ec0d73b466b0be170742fc0
Krieger <kriegen@ccf.org>
2018-06-21 15:59:01 -0400

    Changes to get_reference_area.R

--- R/get_reference_area.R
+++ R/get_reference_area.R
@@ -1,23 +1,40 @@
 get_reference_area <- function(geography=NULL, ...) {
   dots <- eval(substitute(alist(...)))
-
+  
+  # if(length(dots) == 0) {
+  #   ref_area <- "us"
+  # }
+  
   # Throws an error if user included more than one variable identifying the
   # reference area
-
+  
   if(length(dots) > 1 & all(names(dots) %in% c("GEOIDs", "states"))) {
     stop("Reference area may only include one of: GEOIDs, states.")
-  }
-
+  } 
+  
   if("GEOIDs" %in% names(dots)) {
-    ref_area <- parse_GEOIDs(dots[["GEOIDs"]])
+    ref_area <- eval(dots[["GEOIDs"]])
+  } else if("states" %in% names(dots)) {
+    ref_area <- eval(dots[["states"]])
+  } else {
+    message("No valid reference area given. Using entire US as reference area.")
+    ref_area <- "us"
   }
-  else if("states" %in% names(dots)) {
-    ref_area <-
-      acs::fips.state$STATE[acs::fips.state$STUSAB %in% eval(dots[["states"]])]
-    ref_area <- stringr::str_pad(as.character(ref_area),
-                                 width = 2, side = "left", pad = "0")
+  
+  if(geography == "state") {
+    ref_area <- unique(unlist(purrr::map(ref_area,
+                                         tidycensus:::validate_state)))
   }
 
+  ref_area <- dplyr::case_when(
+    geography == "state" ~ tidycensus:::validate_state())
+  ref_area <- parse_GEOIDs(dots[["GEOIDs"]])
+  
+  ref_area <-
+    acs::fips.state$STATE[acs::fips.state$STUSAB %in% eval(dots[["states"]])]
+  ref_area <- stringr::str_pad(as.character(ref_area),
+                               width = 2, side = "left", pad = "0")
+  
   return(ref_area)
 
 }

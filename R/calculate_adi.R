c1ce2aac3ab26b403a61174bc6ecb7f4f73e6894
Krieger <kriegen@lri-gen-p-00.lerner.ccf.org>
2018-07-12 14:33:02 -0400

    Fixed calculate_adi

--- R/calculate_adi.R
+++ R/calculate_adi.R
@@ -1,35 +1,40 @@
 calculate_adi <- function(ref_area, year, survey, key) {
-
+  
+  # acs_data_raw <-
+  #   purrr::map_dfr(ref_area$state_county, call_get_acs,
+  #                  geography = ref_area$geography,
+  #                  year = year, survey = survey, key = key)
+  
   acs_data_raw <-
-    purrr::map_dfr(ref_area[[3]],
-                   function(state_county)
-                     tidycensus::get_acs(
-                       geography = ref_area[[2]],
-                       state = state_county[[1]], county = state_county[[2]],
-                       year = year, survey = survey, key = key,
-                       variables =
-                         c("B01003_001","B19013_001","B19001_002","B19001_011",
-                           "B19001_012","B19001_013","B19001_014","B19001_015",
-                           "B19001_016","B19001_017","B17010_001","B17010_002",
-                           "B25003_001","B25003_002","C17002_001","C17002_002",
-                           "C17002_003","C17002_004","C17002_005","B25044_001",
-                           "B25044_003","B25044_010","B25014_001","B25014_005",
-                           "B25014_006","B25014_007","B25014_011","B25014_012",
-                           "B25014_013","B25088_001","B25064_001","B25077_001",
-                           "C24010_001","C24010_003","C24010_039","B23025_001",
-                           "B23025_005","B15003_001","B15003_002","B15003_003",
-                           "B15003_004","B15003_005","B15003_006","B15003_007",
-                           "B15003_008","B15003_009","B15003_010","B15003_011",
-                           "B15003_012","B15003_017","B15003_018","B15003_019",
-                           "B15003_020","B15003_021","B15003_022","B15003_023",
-                           "B15003_024","B15003_025","B23008_001","B23008_008",
-                           "B23008_021"),
-                       output = "wide", table = NULL, cache_table = TRUE,
-                       geometry = FALSE, keep_geo_vars = FALSE,
-                       shift_geo = FALSE, summary_var = NULL, moe_level = 90))
+    purrr::map_dfr(
+      ref_area$state_county,
+      function(state_county, geography, year, survey, key) {
+        state <- state_county$state
+        county <- state_county$county
+        tidycensus::get_acs(
+          geography = geography, state = state, county = county,
+          year = year, survey = survey, key = key, cache_table = TRUE,
+          variables =
+            c("B01003_001","B19013_001","B19001_002","B19001_011","B19001_012",
+              "B19001_013","B19001_014","B19001_015","B19001_016","B19001_017",
+              "B17010_001","B17010_002","B25003_001","B25003_002","C17002_001",
+              "C17002_002","C17002_003","C17002_004","C17002_005","B25044_001",
+              "B25044_003","B25044_010","B25014_001","B25014_005","B25014_006",
+              "B25014_007","B25014_011","B25014_012","B25014_013","B25088_001",
+              "B25064_001","B25077_001","C24010_001","C24010_003","C24010_039",
+              "B23025_001","B23025_005","B15003_001","B15003_002","B15003_003",
+              "B15003_004","B15003_005","B15003_006","B15003_007","B15003_008",
+              "B15003_009","B15003_010","B15003_011","B15003_012","B15003_017",
+              "B15003_018","B15003_019","B15003_020","B15003_021","B15003_022",
+              "B15003_023","B15003_024","B15003_025","B23008_001","B23008_008",
+              "B23008_021"),
+          output = "wide", table = NULL, geometry = FALSE, shift_geo = FALSE, 
+          keep_geo_vars = FALSE, summary_var = NULL, moe_level = 90)
+        },
+      geography = ref_area$geography, year = year, survey = survey, key = key)
 
   acs_data <- acs_data_raw %>%
-    dplyr::filter(GEOID %in% ref_area[[1]]) %>%
+    dplyr::filter(GEOID %in% ref_area$ref_geoids) %>%
     dplyr::select(GEOID, NAME, B01003_001E, B19013_001E, B19001_002E,
                   B19001_011E, B19001_012E, B19001_013E, B19001_014E,
                   B19001_015E, B19001_016E, B19001_017E, B17010_001E,
@@ -123,3 +128,32 @@ calculate_adi <- function(ref_area, year, survey, key) {
 
   return(acs_adi)
 }
+
+# call_get_acs <- function(state_county, geography, year, survey, key) {
+#   state <- state_county$state
+#   county <- state_county$county
+#   tidycensus::get_acs(
+#     geography = geography,
+#     state = state, county = county,
+#     year = year, survey = survey, key = key,
+#     variables =
+#       c("B01003_001","B19013_001","B19001_002","B19001_011",
+#         "B19001_012","B19001_013","B19001_014","B19001_015",
+#         "B19001_016","B19001_017","B17010_001","B17010_002",
+#         "B25003_001","B25003_002","C17002_001","C17002_002",
+#         "C17002_003","C17002_004","C17002_005","B25044_001",
+#         "B25044_003","B25044_010","B25014_001","B25014_005",
+#         "B25014_006","B25014_007","B25014_011","B25014_012",
+#         "B25014_013","B25088_001","B25064_001","B25077_001",
+#         "C24010_001","C24010_003","C24010_039","B23025_001",
+#         "B23025_005","B15003_001","B15003_002","B15003_003",
+#         "B15003_004","B15003_005","B15003_006","B15003_007",
+#         "B15003_008","B15003_009","B15003_010","B15003_011",
+#         "B15003_012","B15003_017","B15003_018","B15003_019",
+#         "B15003_020","B15003_021","B15003_022","B15003_023",
+#         "B15003_024","B15003_025","B23008_001","B23008_008",
+#         "B23008_021"),
+#     output = "wide", table = NULL, cache_table = TRUE,
+#     geometry = FALSE, keep_geo_vars = FALSE,
+#     shift_geo = FALSE, summary_var = NULL, moe_level = 90)
+# }

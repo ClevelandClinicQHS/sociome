d73c431f3e1d166800487f8f14de339319d0859a
NikKrieger <nxk196@case.edu>
2018-08-15 11:09:47 -0400

    Edit to zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -3,7 +3,7 @@
   us_block_groups <- stringr::str_pad(us_block_groups, width = 12,
                                       side = "left", pad = "0")
   
-  fips_table <<-
+  fips_table <-
     data.frame(`block group` = us_block_groups,
                tract = stringr::str_sub(us_block_groups, 1, 11),
                county = stringr::str_sub(us_block_groups, 1, 5),
@@ -12,6 +12,6 @@
                
                check.names = FALSE,
                stringsAsFactors = FALSE)
-  
-  # return(fips_table)
+  getwd()
+  #save(fips_table, file = "")
 }
